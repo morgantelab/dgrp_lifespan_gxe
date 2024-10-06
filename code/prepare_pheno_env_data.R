389b55bd7d601f02df32183e7205ae9c500920d5
fmorgante <fabiom@clemson.edu>
2024-10-06 17:10:59 -0400

    Minor change

--- code/prepare_pheno_env_data.R
+++ code/prepare_pheno_env_data.R
@@ -8,13 +8,13 @@ dat_M_18 <- read.table("/data2/morgante_lab/data/dgrp/phenotypes/lifespan/male_1
 dat_M_25 <- read.table("/data2/morgante_lab/data/dgrp/phenotypes/lifespan/male_25c_mean_pheno.txt", header=FALSE, sep="\t")
 dat_M_28 <- read.table("/data2/morgante_lab/data/dgrp/phenotypes/lifespan/male_28c_mean_pheno.txt", header=FALSE, sep="\t")
 colnames(dat_F_18) <- colnames(dat_F_25) <- colnames(dat_F_28) <- colnames(dat_M_18) <- colnames(dat_M_25) <- colnames(dat_M_28) <- 
-  c("id", "y")
-rownames(dat_F_18) <- dat_F_18$id
-rownames(dat_F_25) <- dat_F_25$id
-rownames(dat_F_28) <- dat_F_28$id
-rownames(dat_M_18) <- dat_M_18$id
-rownames(dat_M_25) <- dat_M_25$id
-rownames(dat_M_28) <- dat_M_28$id
+  c("line_id", "y")
+rownames(dat_F_18) <- dat_F_18$line_id
+rownames(dat_F_25) <- dat_F_25$line_id
+rownames(dat_F_28) <- dat_F_28$line_id
+rownames(dat_M_18) <- dat_M_18$line_id
+rownames(dat_M_25) <- dat_M_25$line_id
+rownames(dat_M_28) <- dat_M_28$line_id
 
 ###Remove missing phenotypes
 dat_F_18 <- dat_F_18[complete.cases(dat_F_18), ]
@@ -39,7 +39,7 @@ dat_M_28$temp <- 28
 dat_M_28$sex <- 0
 
 ###Get IDs of lines without missing values
-id_count <- table(c(dat_F_18$id, dat_F_25$id, dat_F_28$id, dat_M_18$id, dat_M_25$id, dat_M_28$id))
+id_count <- table(c(dat_F_18$line_id, dat_F_25$line_id, dat_F_28$line_id, dat_M_18$line_id, dat_M_25$line_id, dat_M_28$line_id))
 ids_complete_cases <- names(which(id_count == 6))
 
 ###Merge datasets
@@ -50,6 +50,9 @@ dat <- rbind(dat_F_18[ids_complete_cases, ],
              dat_M_25[ids_complete_cases, ],
              dat_M_28[ids_complete_cases, ])
 rownames(dat) <- 1:nrow(dat)
+
+###Create id of the observation
+dat$obs_id <- paste0(dat$line_id, "_", dat$temp, "_", dat$sex)
 
 ###Save data
 saveRDS(dat, file="../data/pheno_env.rds")
